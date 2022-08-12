3eec88fe309fa163bb3922aa7fbd81ca5d7babc7
foxninetailsca <36778918+foxninetailsca@users.noreply.github.com>
2022-08-11 22:17:44 -0300

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,10 @@
+<?php 
+		$cardapio = array(
+			0 => array('id'=>'1', 'nome'=>'bife acebolado', 'tamanho'=>'750g', 'preco'=>'R$ 12,00', 'imagem'=>'', 'ingredientes'=>'' ),
+			1 => array('id'=>'2', 'nome'=>'macarrão com frango', 'tamanho'=>'700g', 'preco'=>'R$ 10,00', 'imagem'=>'', 'ingredientes'=>'' ),
+			2 => array('id'=>'3', 'nome'=>'lasanha', 'tamanho'=>'1kg', 'preco'=>'R$ 20,00', 'imagem'=>'', 'ingredientes'=>'' ),
+			3 => array('id'=>'4', 'nome'=>'carne com legumes', 'tamanho'=>'600g', 'preco'=>'R$ 12,00', 'imagem'=>'', 'ingredientes'=>'' ),
+			4 => array('id'=>'5', 'nome'=>'strogonoff', 'tamanho'=>'750g', 'preco'=>'R$ 20,00', 'imagem'=>'', 'ingredientes'=>'' ),
+			5 => array('id'=>'6', 'nome'=>'feijoada', 'tamanho'=>'1kg', 'preco'=>'R$ 20,00', 'imagem'=>'', 'ingredientes'=>'' ),
+		)
+	?>
